bed40ac92949e413c87557d03ed603439b5ac6a4
bekhruzRakhmonov <85542200+bekhruzRakhmonov@users.noreply.github.com>
2021-12-07 00:47:39 +0500

    make it better

--- settings.py
+++ settings.py
@@ -10,8 +10,8 @@ HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 
 
 # webhook settings
-WEBHOOK_HOST = 'https://igviddwdbot.herokuapp.com'
-WEBHOOK_PATH = '/webhook/5075815121:AAHowhsyJHRxik-AfWsUjXdzITlp6iz_vuc'
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
 # webserver settings
