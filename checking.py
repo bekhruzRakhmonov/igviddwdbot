c3ada934f6a2e796de9ec96027d68a2d406cd64b
bekhruzRakhmonov <85542200+bekhruzRakhmonov@users.noreply.github.com>
2022-05-31 14:20:54 +0500

    make it better

--- checking.py
+++ checking.py
@@ -1,5 +1,5 @@
 from config import bot,dp
-import requests
+import requests as r
 import re
 import os
 import cgitb
@@ -9,19 +9,18 @@ from urllib.parse import urlparse
 cgitb.enable()
 
 def is_valid(url):
-	path = urlparse(url).path
-	prepared_url = f"https://www.instagram.com/{path}"
-	req = requests.get(prepared_url)
-	if req.status_code == 200:
-		return True
+	if re.match(r"https://www.instagram.com/tv/",url) or re.match(r"https://www.instagram.com/p/",url) or re.match(r"https://www.instagram.com/reel/",url) :
+		req = r.get(url)
+		if req.status_code == 200:
+			return True
 	else:
 		return False
-
 def prepare_urls(matches):
 	return list({match.replace("\\u0026", "&") for match in matches})
 
 async def send_video(user,viewers,username,description):
-	splited_viewers = viewers[0].split(',')
-	await bot.send_video(user.id,open(f'videos/{user.id}.mp4','rb'),caption=f"<b>Viewers count</b>: <i>{splited_viewers[0]}</i>\n@igviddwdbot",parse_mode='HTML')
+	print(viewers)
+	viewers_count = viewers[0].strip(',')
+	await bot.send_video(user.id,open(f'videos/{user.id}.mp4','rb'),caption=f"<b>Viewers count</b>: <i>{viewers_count}</i>\n@igviddwdbot",parse_mode='HTML')
 	# if os.path.exists(f"videos/{user.id}.mp4"):
 		# os.remove(f"videos/{user.id}.mp4")
