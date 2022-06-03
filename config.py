61d3b12655449cba0dd0f8e924cb20e16a70c8ae
bekhruzRakhmonov <85542200+bekhruzRakhmonov@users.noreply.github.com>
2022-06-03 12:06:07 +0500

    make it better

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import logging
 from aiogram import Bot, Dispatcher, executor, types
 # from settings import BOT_TOKEN
 import os
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
 logging.basicConfig(level=logging.INFO)
 
